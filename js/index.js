63953a9c5f87edef35f47a5a40421d7b35be8966
SoleMina <karina.pradogutierrez@gmail.com>
2021-03-12 21:53:10 -0500

    add delete function

--- js/index.js
+++ js/index.js
@@ -4,6 +4,26 @@ const inputTitle = document.querySelector(".title-form");
 const textarea = document.querySelector("textarea");
 const btnAdd = document.querySelector(".btn-note");
 const noteBox = document.querySelector(".notes-box");
+//Add Note function - Create the html box
+function addNote(noteTitle, noteText) {
+  const item = `<div class="box">
+                      <h4>${noteTitle}</h4>
+                      <p>${noteText}</p>
+                      <button class="btn btn-delete">Remove</button>
+                  </div>`;
+
+  const position = "beforeend";
+
+  noteBox.insertAdjacentHTML(position, item);
+
+  let btnDelete = document.querySelectorAll(".btn-delete");
+
+  for (let i = 0; i < btnDelete.length; i++) {
+    btnDelete[i].addEventListener("click", function () {
+      btnDelete[i].parentElement.style.display = "none";
+    });
+  }
+}
 
 btnAdd.addEventListener("click", function () {
   let title = inputTitle.value;
@@ -13,33 +33,3 @@ btnAdd.addEventListener("click", function () {
   inputTitle.value = "";
   textarea.value = "";
 });
-
-//Add to do function
-function addNote(noteTitle, noteText) {
-  const item = `<div class="box">
-                    <h4>${noteTitle}</h4>
-                    <p>${noteText}</p>
-                    <button class="btn btn-delete">Remove</button>
-                </div>`;
-
-  const position = "beforeend";
-
-  noteBox.insertAdjacentHTML(position, item);
-}
-
-//
-//Setting variables
-
-// const inputTitle = document.querySelector(".title-form");
-// const textarea = document.querySelector("textarea");
-// const btnAdd = document.querySelector(".btn-note");
-// const noteBox = document.querySelector(".notes-box");
-
-// btnAdd.addEventListener("click", function () {
-//   addNote();
-// });
-
-// //Add to do function
-// function addNote() {
-//   noteBox.insertAdjacentHTML("beforeend", "<a>Item 1</a");
-// }
